03cb52a57898eb28cc7556c91fc4979771c7a39c
esticae <esticaeugene@gmail.com>
2024-06-11 10:16:20 -0400

    new script

---
+++ db_script.sql
@@ -0,0 +1,10 @@
+DROP DATABASE databasename;
+CREATE DATABASE databasename;
+CREATE TABLE dealerships (
+    Dealership_id int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(50),
+    Address varchar(50),
+    Phone varchar(50)
+);
+
+
